c38037bfbcfd655977157f4d7cb893292d065b76
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-06-21 13:41:56 -0400

    Aula 16 - User Registration Part-B

--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -71,10 +73,10 @@ router.post('/register', function(req, res, next) {
 		});
 
 		// Create User
-		// User.createUser(newUser, function(err, user){
-		//	if (err) throw err;
-		//	console.log(user);
-		// });
+		User.createUser(newUser, function(err, user){
+			if (err) throw err;
+			console.log(user);
+		});
 
 		// Success message
 		req.flash('success', 'You are registered and may log in');
