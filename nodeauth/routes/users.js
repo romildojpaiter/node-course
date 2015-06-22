faaf32107130e095a0c199f0711bce5cf680939e
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-06-22 00:11:39 -0400

    Aula 19 - Logout & Access Control A

--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -130,4 +130,10 @@ router.post('/login', passport.authenticate('local',
 		}
 );
 
+router.get('/logout', function(req, res) {
+	req.logout;
+	req.flash('success', 'You have logged out');
+	res.redirect('/users/login');
+});
+
 module.exports = router;
